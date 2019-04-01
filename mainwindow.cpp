6bf10d1dc67e597282ddb0a40b29a703516e3230
Ptilulu83 <lucas.moragues@wanadoo.fr>
2019-04-01 11:56:33 +0200

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,279 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+void MainWindow::showParts(MyMesh* _mesh)
+{
+    int colors[] ={
+        255,    0,      0,      //objet0
+        0,      255,    0,      //objet1
+        0,      0,      255,    //objet2
+        0,      255,    255,    //objet3
+        120,    0,      200,    //objet4
+        0,      0,      0,      //objet5
+    };
+    int num_objet=0;
+
+    int start_point=0;
+
+    int nb_vertex=_mesh->n_vertices();
+    bool marked[nb_vertex];
+    for (int i=0;i<nb_vertex;++i) {
+        marked[i]=false;
+    }
+    QVector<int> liste;
+    liste.append(start_point);
+
+    bool is_done=false;
+
+    while(!is_done){
+        is_done=true;
+
+        //on parcours tout les voisins du sommet de départ
+        while(liste.size() > 0 ){
+            //on récupert le sommet de départ
+            VertexHandle current_vertex=_mesh->vertex_handle(liste.back());
+            marked[liste.back()]=true;
+            liste.pop_back();
+
+            //changer couleur face adjacente de current_vertex
+            for (MyMesh::VertexFaceIter curFace = _mesh->vf_iter(current_vertex); curFace.is_valid(); curFace++){
+                _mesh->set_color(*curFace, MyMesh::Color(colors[num_objet*3+0], colors[num_objet*3+1], colors[num_objet*3+2]));
+            }
+
+            //ajouter les voisins non marquer de current_vertex a la liste
+            for (MyMesh::VertexVertexIter curVert = _mesh->vv_iter(current_vertex); curVert.is_valid(); curVert++){
+                int new_vertex_id=curVert->idx();
+                if(!marked[new_vertex_id]){
+                    marked[new_vertex_id]=true;
+                    liste.append(new_vertex_id);
+                }
+            }
+        }
+
+        // on verifie que tout les sommet ont été parcouru si non il existe un autre objet
+        for(int i=0;i<nb_vertex;++i) {
+            if(!marked[i]){
+                is_done=false;
+                num_objet++;
+                if(num_objet > 5){
+                    num_objet=5;
+                }
+                liste.append(i);
+                break;
+            }
+        }
+    }
+
+}
+
+
+/* **** début de la partie boutons et IHM **** */
+void MainWindow::on_pushButton_clicked()
+{
+    // on réinitialise l'affichage
+    resetAllColorsAndThickness(&mesh);
+
+    showParts(&mesh);
+    
+    // on affiche le nouveau maillage
+    displayMesh(&mesh);
+}
+
+
+void MainWindow::on_pushButton_chargement_clicked()
+{
+    // fenêtre de sélection des fichiers
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Mesh"), "", tr("Mesh Files (*.obj)"));
+
+    // chargement du fichier .obj dans la variable globale "mesh"
+    OpenMesh::IO::read_mesh(mesh, fileName.toUtf8().constData());
+
+    // initialisation des couleurs et épaisseurs (sommets et arêtes) du mesh
+    resetAllColorsAndThickness(&mesh);
+
+    // on affiche le maillage
+    displayMesh(&mesh);
+}
+/* **** fin de la partie boutons et IHM **** */
+
+
+
+/* **** fonctions supplémentaires **** */
+// permet d'initialiser les couleurs et les épaisseurs des élements du maillage
+void MainWindow::resetAllColorsAndThickness(MyMesh* _mesh)
+{
+    for (MyMesh::VertexIter curVert = _mesh->vertices_begin(); curVert != _mesh->vertices_end(); curVert++)
+    {
+        _mesh->data(*curVert).thickness = 1;
+        _mesh->set_color(*curVert, MyMesh::Color(0, 0, 0));
+    }
+
+    for (MyMesh::FaceIter curFace = _mesh->faces_begin(); curFace != _mesh->faces_end(); curFace++)
+    {
+        _mesh->set_color(*curFace, MyMesh::Color(150, 150, 150));
+    }
+
+    for (MyMesh::EdgeIter curEdge = _mesh->edges_begin(); curEdge != _mesh->edges_end(); curEdge++)
+    {
+        _mesh->data(*curEdge).thickness = 1;
+        _mesh->set_color(*curEdge, MyMesh::Color(0, 0, 0));
+    }
+}
+
+// charge un objet MyMesh dans l'environnement OpenGL
+void MainWindow::displayMesh(MyMesh* _mesh)
+{
+    GLuint* triIndiceArray = new GLuint[_mesh->n_faces() * 3];
+    GLfloat* triCols = new GLfloat[_mesh->n_faces() * 3 * 3];
+    GLfloat* triVerts = new GLfloat[_mesh->n_faces() * 3 * 3];
+
+    MyMesh::ConstFaceIter fIt(_mesh->faces_begin()), fEnd(_mesh->faces_end());
+    MyMesh::ConstFaceVertexIter fvIt;
+    int i = 0;
+    for (; fIt!=fEnd; ++fIt)
+    {
+        fvIt = _mesh->cfv_iter(*fIt);
+        triCols[3*i+0] = _mesh->color(*fIt)[0]; triCols[3*i+1] = _mesh->color(*fIt)[1]; triCols[3*i+2] = _mesh->color(*fIt)[2];
+        triVerts[3*i+0] = _mesh->point(*fvIt)[0]; triVerts[3*i+1] = _mesh->point(*fvIt)[1]; triVerts[3*i+2] = _mesh->point(*fvIt)[2];
+        triIndiceArray[i] = i;
+
+        i++; ++fvIt;
+        triCols[3*i+0] = _mesh->color(*fIt)[0]; triCols[3*i+1] = _mesh->color(*fIt)[1]; triCols[3*i+2] = _mesh->color(*fIt)[2];
+        triVerts[3*i+0] = _mesh->point(*fvIt)[0]; triVerts[3*i+1] = _mesh->point(*fvIt)[1]; triVerts[3*i+2] = _mesh->point(*fvIt)[2];
+        triIndiceArray[i] = i;
+
+        i++; ++fvIt;
+        triCols[3*i+0] = _mesh->color(*fIt)[0]; triCols[3*i+1] = _mesh->color(*fIt)[1]; triCols[3*i+2] = _mesh->color(*fIt)[2];
+        triVerts[3*i+0] = _mesh->point(*fvIt)[0]; triVerts[3*i+1] = _mesh->point(*fvIt)[1]; triVerts[3*i+2] = _mesh->point(*fvIt)[2];
+        triIndiceArray[i] = i;
+
+        i++;
+    }
+
+    ui->displayWidget->loadMesh(triVerts, triCols, _mesh->n_faces() * 3 * 3, triIndiceArray, _mesh->n_faces() * 3);
+
+    delete[] triIndiceArray;
+    delete[] triCols;
+    delete[] triVerts;
+
+    GLuint* linesIndiceArray = new GLuint[_mesh->n_edges() * 2];
+    GLfloat* linesCols = new GLfloat[_mesh->n_edges() * 2 * 3];
+    GLfloat* linesVerts = new GLfloat[_mesh->n_edges() * 2 * 3];
+
+    i = 0;
+    QHash<float, QList<int> > edgesIDbyThickness;
+    for (MyMesh::EdgeIter eit = _mesh->edges_begin(); eit != _mesh->edges_end(); ++eit)
+    {
+        float t = _mesh->data(*eit).thickness;
+        if(t > 0)
+        {
+            if(!edgesIDbyThickness.contains(t))
+                edgesIDbyThickness[t] = QList<int>();
+            edgesIDbyThickness[t].append((*eit).idx());
+        }
+    }
+    QHashIterator<float, QList<int> > it(edgesIDbyThickness);
+    QList<QPair<float, int> > edgeSizes;
+    while (it.hasNext())
+    {
+        it.next();
+
+        for(int e = 0; e < it.value().size(); e++)
+        {
+            int eidx = it.value().at(e);
+
+            MyMesh::VertexHandle vh1 = _mesh->to_vertex_handle(_mesh->halfedge_handle(_mesh->edge_handle(eidx), 0));
+            linesVerts[3*i+0] = _mesh->point(vh1)[0];
+            linesVerts[3*i+1] = _mesh->point(vh1)[1];
+            linesVerts[3*i+2] = _mesh->point(vh1)[2];
+            linesCols[3*i+0] = _mesh->color(_mesh->edge_handle(eidx))[0];
+            linesCols[3*i+1] = _mesh->color(_mesh->edge_handle(eidx))[1];
+            linesCols[3*i+2] = _mesh->color(_mesh->edge_handle(eidx))[2];
+            linesIndiceArray[i] = i;
+            i++;
+
+            MyMesh::VertexHandle vh2 = _mesh->from_vertex_handle(_mesh->halfedge_handle(_mesh->edge_handle(eidx), 0));
+            linesVerts[3*i+0] = _mesh->point(vh2)[0];
+            linesVerts[3*i+1] = _mesh->point(vh2)[1];
+            linesVerts[3*i+2] = _mesh->point(vh2)[2];
+            linesCols[3*i+0] = _mesh->color(_mesh->edge_handle(eidx))[0];
+            linesCols[3*i+1] = _mesh->color(_mesh->edge_handle(eidx))[1];
+            linesCols[3*i+2] = _mesh->color(_mesh->edge_handle(eidx))[2];
+            linesIndiceArray[i] = i;
+            i++;
+        }
+        edgeSizes.append(qMakePair(it.key(), it.value().size()));
+    }
+
+    ui->displayWidget->loadLines(linesVerts, linesCols, i * 3, linesIndiceArray, i, edgeSizes);
+
+    delete[] linesIndiceArray;
+    delete[] linesCols;
+    delete[] linesVerts;
+
+    GLuint* pointsIndiceArray = new GLuint[_mesh->n_vertices()];
+    GLfloat* pointsCols = new GLfloat[_mesh->n_vertices() * 3];
+    GLfloat* pointsVerts = new GLfloat[_mesh->n_vertices() * 3];
+
+    i = 0;
+    QHash<float, QList<int> > vertsIDbyThickness;
+    for (MyMesh::VertexIter vit = _mesh->vertices_begin(); vit != _mesh->vertices_end(); ++vit)
+    {
+        float t = _mesh->data(*vit).thickness;
+        if(t > 0)
+        {
+            if(!vertsIDbyThickness.contains(t))
+                vertsIDbyThickness[t] = QList<int>();
+            vertsIDbyThickness[t].append((*vit).idx());
+        }
+    }
+    QHashIterator<float, QList<int> > vitt(vertsIDbyThickness);
+    QList<QPair<float, int> > vertsSizes;
+
+    while (vitt.hasNext())
+    {
+        vitt.next();
+
+        for(int v = 0; v < vitt.value().size(); v++)
+        {
+            int vidx = vitt.value().at(v);
+
+            pointsVerts[3*i+0] = _mesh->point(_mesh->vertex_handle(vidx))[0];
+            pointsVerts[3*i+1] = _mesh->point(_mesh->vertex_handle(vidx))[1];
+            pointsVerts[3*i+2] = _mesh->point(_mesh->vertex_handle(vidx))[2];
+            pointsCols[3*i+0] = _mesh->color(_mesh->vertex_handle(vidx))[0];
+            pointsCols[3*i+1] = _mesh->color(_mesh->vertex_handle(vidx))[1];
+            pointsCols[3*i+2] = _mesh->color(_mesh->vertex_handle(vidx))[2];
+            pointsIndiceArray[i] = i;
+            i++;
+        }
+        vertsSizes.append(qMakePair(vitt.key(), vitt.value().size()));
+    }
+
+    ui->displayWidget->loadPoints(pointsVerts, pointsCols, i * 3, pointsIndiceArray, i, vertsSizes);
+
+    delete[] pointsIndiceArray;
+    delete[] pointsCols;
+    delete[] pointsVerts;
+}
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    vertexSelection = -1;
+    edgeSelection = -1;
+    faceSelection = -1;
+
+    modevoisinage = false;
+
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
